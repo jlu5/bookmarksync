3bc2544a1ea5eb2f9c5cae6b4f966a2b0a8c3ce9
James Lu <james@overdrivenetworks.com>
2021-10-24 01:50:54 -0700

    qt: force loading the built-in QFileDialog

--- src/bookmarksyncqtbackend.cpp
+++ src/bookmarksyncqtbackend.cpp
@@ -10,6 +10,9 @@ BookmarkSyncQtBackend::BookmarkSyncQtBackend(BookmarkSync* parent, BackendWidget
     listView->setModel(model);
 
     dialog = new QFileDialog(widget);
+    // Have to explicitly use the built-in Qt dialog for this to get populated
+    // see https://github.com/qt/qtbase/blob/91534587314791f0d6ad7866308e0d22e2794fc9/src/widgets/dialogs/qfiledialog.cpp#L436
+    dialog->setOption(QFileDialog::DontUseNativeDialog);
     loadPlaces();
 
     QObject::connect(widget->addButton, &QAbstractButton::clicked, this, &BookmarkSyncQtBackend::onAddButtonClicked);
@@ -25,7 +28,6 @@ void BookmarkSyncQtBackend::loadPlaces() {
     QVector<Place> places;
     for (QUrl& url: urls) {
         // Looks like Qt does not support custom labels, so just use the basename
-        // TODO: does this support non-local files?
         QString label = url.path().section("/", -1);
         places.append(Place{label, url});
     }
